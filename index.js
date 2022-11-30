a82d6f6446a00a49d71f1f4917a90b09e9235a0c
semabraham99 <semaw190699@gmail.com>
2022-11-30 14:22:25 +0700

    commit test 1

---
+++ index.js
@@ -0,0 +1,6 @@
+import { AppRegistry } from "react-native";
+import HalamanUtama from '.App';
+import {name as appName} from '.app.json';
+
+
+AppRegistry.registerComponent (appName, () => HalamanUtama);
